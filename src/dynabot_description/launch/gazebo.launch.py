208a54b3cc8dbdec866f125208a306faa4ce4f2f
axel-ben <andybenitez30@yahoo.com>
2025-03-31 19:43:55 -0700

    Added support for Ros 2 Control

--- src/dynabot_description/launch/gazebo.launch.py
+++ src/dynabot_description/launch/gazebo.launch.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -12,59 +12,61 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-    # Get the package share directory, variable is used to get the path to the model file
-    dynabot_desription_dir = get_package_share_directory("dynabot_description")
+    dynabot_description = get_package_share_directory("dynabot_description")
 
-    # Declare the model argument, it is used to get the path to the model file
-    model_arg = DeclareLaunchArgument(
-        name="model",
-        default_value=os.path.join(dynabot_desription_dir,"description", "dynabot_gazebo.xacro"),
-        description="path to the model file"
-    )
-    
-    # Declare the robot description parameter, it is used to get the path to the model file
-    robot_description = ParameterValue(Command([
-        'xacro ', LaunchConfiguration('model')]),
-        value_type=str
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        dynabot_description, "description", "dynabot.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file"
     )
 
-    # Declare the robot state publisher node    
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher",
-        parameters=[{"robot_description": robot_description}]
-    )
-
-    # Declare the gazebo resource path environment variable
     gazebo_resource_path = SetEnvironmentVariable(
         name="GZ_SIM_RESOURCE_PATH",
         value=[
-            str(Path(dynabot_desription_dir).parent.resolve())
-        ]
+            str(Path(dynabot_description).parent.resolve())
+            ]
+        )
+    
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "humble" else "False"
+    
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
+        ]),
+        value_type=str
     )
 
-    # Declare the gazebo launch file
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
+    )
+
     gazebo = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py")
-            ),
-            launch_arguments=[
-                ("gz_args",[" -v 4"," -r"," empty.sdf"])
-            ]
-        )    
-    
-    # Declare the spawn entity node
-    spawn_entity = Node(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments=[
+                    ("gz_args", [" -v 4", " -r", " empty.sdf"]
+                    )
+                ]
+             )
+
+    gz_spawn_entity = Node(
         package="ros_gz_sim",
         executable="create",
-        arguments=["-topic", "/robot_description", "-name", "dynabot"]
-        )
+        output="screen",
+        arguments=["-topic", "robot_description",
+                   "-name", "dynabot"],
+    )
 
     return LaunchDescription([
         model_arg,
-        robot_state_publisher_node,
         gazebo_resource_path,
+        robot_state_publisher_node,
         gazebo,
-        spawn_entity
+        gz_spawn_entity
     ])
