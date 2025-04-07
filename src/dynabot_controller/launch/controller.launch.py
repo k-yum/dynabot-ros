147dd596b85c152c29a4e9df00eb918fefaa1e13
axel-ben <andybenitez30@yahoo.com>
2025-04-06 20:02:34 -0700

    Added support for diff_drive_controller pkg

--- src/dynabot_controller/launch/controller.launch.py
+++ src/dynabot_controller/launch/controller.launch.py
@@ -1,7 +1,27 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="True",
+    )
+    
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius",
+        default_value="0.202",
+    )
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation",
+        default_value="0.66890",
+    )
+    
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_separation = LaunchConfiguration("wheel_separation")
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -13,18 +33,21 @@ def generate_launch_description():
         ],
     )
 
-    #ros2 node for velocity controller
-    simple_controller = Node(
+    diff_drive_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=[
-            "simple_velocity_controller",
-            "--controller-manager",
-            "/controller_manager",
+        arguments=["diff_drive_controller", 
+                   "--controller-manager", 
+                   "/controller_manager"
         ],
     )
 
-    return LaunchDescription([
-        joint_state_broadcaster_spawner,
-        simple_controller,
-    ]) 
+    return LaunchDescription(
+        [
+            use_sim_time_arg,
+            wheel_radius_arg,
+            wheel_separation_arg,
+            joint_state_broadcaster_spawner,
+            diff_drive_controller_spawner,
+        ]
+    )
